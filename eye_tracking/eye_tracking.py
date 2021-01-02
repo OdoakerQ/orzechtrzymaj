7bc96a865a7af252692f76ff7addf59fe4f78962
OdoakerQ <kubakowieski5151@gmail.com>
2021-01-02 19:41:08 +0100

    Added possibility to modify fps

---
+++ eye_tracking/eye_tracking.py
@@ -0,0 +1,46 @@
+import numpy as np
+import cv2
+import time
+from gaze_tracking import GazeTracking
+
+cap = cv2.VideoCapture(0)
+
+
+MAX_FPS = cap.get(cv2.CAP_PROP_FPS)
+
+fps = 30
+passed = float(0)
+prev = time.time()
+
+gaze = GazeTracking()
+
+while(True):
+    if passed >= (1 / fps):
+
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        gaze.refresh(frame)
+
+        if gaze.pupils_located:
+            text = "Eyes found"
+        else:
+            text = "Eyes 404"
+
+        cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
+        frame = gaze.annotated_frame()
+
+
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+        prev = t
+    t = time.time()
+    passed = t - prev
+    print(passed)
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
